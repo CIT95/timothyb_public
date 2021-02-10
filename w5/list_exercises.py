d8385a927ac6ef1f16d4ae8c7ff0c4a26453d611
Tim Behl <mrtimbehl@gmail.com>
2021-02-10 13:06:38 -0800

    finished week 5 learn together

---
+++ w5/list_exercises.py
@@ -0,0 +1,94 @@
+# Exercise 1
+lst=[11, 100, 99, 1000, 999]
+answer_1=lst[0]
+
+print(answer_1)
+
+# Exercise 2
+lst=[11, 100, 101, 999, 1001]
+
+print(lst[1])
+
+# Exercise 3
+lst=[11, 100, 101, 999, 1001]
+
+answer_1=lst[4]
+
+print(answer_1)
+
+# Exercise 4
+gift_list=['socks', '4K drone', 'wine', 'jam']
+gift_list.append('pajamas')
+
+
+print(gift_list)
+
+# Exercise 5
+gift_list=['socks', '4K drone', 'wine', 'jam']
+gift_list.append(['socks', 'tshirt', 'pajamas'])
+
+
+print(gift_list)
+
+# Exercise 6
+gift_list=['socks', '4K drone', 'wine', 'jam']
+gift_list.insert(2,'slippers')
+
+
+print(gift_list)
+
+# Exercise 7
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+
+answer_1=lst.index(8679)
+
+print(answer_1)
+
+# Exercise 8
+lst=["CRV", "Outback", "XC90", "GL", "Cherokee", "Escalade"]
+lst.append(['Navigator', 'Suburban'])
+
+
+print(lst)
+
+# Exercise 9
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+lst.remove(99)
+
+
+print(lst)
+
+# Exercise 10
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+lst.reverse()
+
+
+print(lst)
+
+# Exercise 11
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+answer_1=lst.count(6)
+
+print(answer_1)
+
+# Exercise 12
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+answer_1=sum(lst)
+
+print(answer_1)
+
+# Exercise 13
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+answer_1=min(lst)
+
+print(answer_1)
+
+# Exercise 14
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+answer_1=max(lst)
+
+print(answer_1)
