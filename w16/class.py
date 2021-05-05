e583636eb1f8a56225e711585dff39bf703fe756
Tim Behl <mrtimbehl@gmail.com>
2021-05-05 14:02:44 -0700

    properly finished w16 LT, adjusted to fit dev criteria

--- w16/class.py
+++ w16/class.py
@@ -1,24 +1,55 @@
-class Soldier:
-    def __init__(self, name, weapon):
+class Player:
+    def __init__(self, name):
         self.name = name
-        self.weapon = weapon
-
-    def attack(self):
-        print(f"{self.name} attacks with their{self.weapon}!")
+        self.hand = []
 
 
-class Knight(Soldier):
-    def __init__(self, name, weapon, shield):
-        super().__init__(name, weapon)
-        self.shield = shield
+class HandLimitPlayer(Player):
+    def __init__(self, name, limit):
+        super().__init__(name)
+        self.limit = limit
 
-    def attack(self):
-        print(f"Knight {self.name} slashes with their {self.weapon}!")
+    def draw(self, card):
+        if len(self.hand) < self.limit:
+            self.hand.append(card)
+            return True
+        return False
 
-    def defend(self):
-        print(f"Knight {self.name} defends with their {self.shield}!")
+
+class ScoreLimitPlayer(Player):
+    def __init__(self, name, limit):
+        super().__init__(name)
+        self.limit = limit
+
+    def draw(self, card):
+        global valueHand
+        if valueHand(self.hand) < self.limit:
+            self.hand.append(card)
+            return True
+        return False
 
 
-class Archer(Soldier):
-    def attack(self):
-        print(f"Archer {self.name} shoots with their {self.weapon}!")
+# Placeholder of valueHand function in actual dev
+def valueHand(hand):
+    return 10
+
+
+theo = HandLimitPlayer("Theodore", 3)
+ronald = ScoreLimitPlayer("Ronald", 18)
+
+if theo.draw("Ace of Spades"):
+    print("Theodore drew a card.")
+if theo.draw("Ace of Clubs"):
+    print("Theodore drew a card.")
+if theo.draw("Ace of Diamonds"):
+    print("Theodore drew a card.")
+if theo.draw("Ace of Hearts"):
+    print("Theodore drew a card.")
+print(theo.hand)
+
+if ronald.draw("Ten of Clubs"):
+    print("Ronald drew a card.")
+ronald.limit = 9
+if ronald.draw("Ten of Spades"):
+    print("Ronald drew a card.")
+print(ronald.hand)
