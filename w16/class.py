0b250fc5f1f632e4e535064b3763a199958d0cac
Tim Behl <mrtimbehl@gmail.com>
2021-05-05 13:49:13 -0700

    finished Week 16 Learn Together

---
+++ w16/class.py
@@ -0,0 +1,27 @@
+class Soldier:
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+
+    def attack(self):
+        print(f"{self.name} attacks with their{self.weapon}!")
+
+
+class Knight(Soldier):
+    def __init__(self, name, weapon, shield):
+        super().__init__(name, weapon)
+        self.shield = shield
+
+    def attack(self):
+        print(f"Knight {self.name} slashes with their {self.weapon}!")
+
+    def defend(self):
+        print(f"Knight {self.name} defends with their {self.shield}!")
+
+
+class Archer(Soldier):
+    def __init__(self, name, weapon):
+        super().__init__(name, weapon)
+
+    def attack(self):
+        print(f"Archer {self.name} shoots with their {self.weapon}!")
